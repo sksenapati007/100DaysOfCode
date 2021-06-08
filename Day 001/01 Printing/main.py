557dd141f075cc46a1422ff790c606051c4070dd
shubhendu <shubhendukumars09@gmail.com>
2021-06-08 23:19:00 +0530

    string concatenation

--- Day 001/01 Printing/main.py
+++ Day 001/01 Printing/main.py
@@ -1,3 +1,7 @@
 
 print("The function is declared like this:")
 print("print('what to print')")
+# New Line character
+print("Hello World!\nHello World!\nHello World!")
+# Concatenate Strings
+print("Hello World!"+" I am learning python...")
