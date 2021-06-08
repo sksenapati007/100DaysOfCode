f1e29c3a5ffb2371b2b13ee380db0c665f323586
shubhendu <shubhendukumars09@gmail.com>
2021-06-08 14:50:28 +0530

    script added

---
+++ docs/js/script.js
@@ -0,0 +1,43 @@
+const getCard = (name, description, link) => {
+    var newNode = document.createElement("div");
+    newNode.className = "content col-3";
+    newNode.innerHTML = `<a class="card col" href="#!" style="border: 0px; background: none">
+    <div class="front" style="background-image: url(images/coding.gif);">
+        <p class="text-danger display-3">${name}</p>
+    </div>
+    <div class="back">
+        <div>
+            <p>${description}</p>
+            <!--<button class="button"
+                onclick="location.href='https://github.com/sksenapati007/100DaysOfCode/'">
+                Fork Me
+            </button>-->
+            <button class="button"
+                onclick=location.href="${ link }">
+                View Project
+            </button>
+        </div>
+    </div>
+  </a>`;
+    return newNode;
+  };
+  
+  const displayProjects = (projects) => {
+    const cardWrapper = document.getElementById("card-wrapper");
+    projects.forEach((project) => {
+      cardWrapper.appendChild(
+        getCard(project.name, project.description, project.link)
+      );
+    });
+  };
+  
+  // Fetch data from json file
+  const projectJsonFile =
+    "https://raw.githubusercontent.com/sksenapati007/100-Days-of-Code/master/docs/data/projects.json";
+  
+  fetch(projectJsonFile)
+    .then((data) => data.json())
+    .then((data) => {
+      displayProjects(data.projects);
+    });
+  
