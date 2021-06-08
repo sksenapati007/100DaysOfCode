775029b1c5d09b097b4d47897c11a55907aff873
shubhendu <shubhendukumars09@gmail.com>
2021-06-08 23:42:39 +0530

    input function done

---
+++ Day 001/03 Input/main.py
@@ -0,0 +1,13 @@
+#Write your code below this line 👇
+
+# input() takes a prompt and returns the user input.
+# then print() prints Hello 
+# concatenating the returned string from input()
+print("Hello "+ input("Whats ur name?")  +" !\n\n")
+
+# input should be a string and return length of that string
+print(len(input("What is ur name?")))
+
+
+
+
