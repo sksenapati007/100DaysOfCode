1fd0cc0fc70d9c460d175f96d6d1e9feb69dcbab
shubhendu <shubhendukumars09@gmail.com>
2021-06-09 22:29:48 +0530

    data types done

---
+++ Day 002/01 DataTypes/main.py
@@ -0,0 +1,50 @@
+#Data Types
+
+# Strings
+# Subscript get a particular index character from the string using [index_number]
+print("Shubhendu"[8])
+
+print("123" + "345")
+
+# Integer
+print(123 + 345)
+
+# to put , 
+print(123_456)
+
+# Float
+print(3.14159)
+
+# Boolean
+print(True)
+
+# to check the type of data
+num_char = len(input("Enter your Name.."))
+print(type(num_char))
+
+# Typecasting (to solve typeError in the below code)
+
+new_num_char =str(num_char)
+print("ur name has "+new_num_char+" characters.")
+
+
+a = float(123)
+print(type(a))
+
+
+print(70 + float("100.5"))
+print(str(70)+str(100))
+
+num = int(input("Enter a 2 digit number :"))
+add = int(num%10)+int(num/10)
+print(add)
+
+#  OR
+
+two_digit_num = input("Enter a 2 digit number :")
+s_digit = two_digit_num[0]
+f_digit = two_digit_num[1]
+
+result = int(s_digit) + int(f_digit)
+
+print(result)
